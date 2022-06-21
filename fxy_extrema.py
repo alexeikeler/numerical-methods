3566162ecc66874064aaab79fbfb4cd24ca9f4fb
alexei <keleralexei@gmail.com>
2022-06-21 23:48:29 +0300

    Formatting text output and optimising derivitive calculation.

--- fxy_extrema.py
+++ fxy_extrema.py
@@ -6,73 +6,101 @@ from tabulate import tabulate
 
 def main():
 
-    n = int(input('Amount of variables: '))
-    f = input('f = ')
-    # n = 3
-    # f = 'x1 ^ 3 + x2 ^ 2 + x3 ^ 2 + x2 * x3 - 3 * x1 + 6 * x2 + 2'
-    # n = 2
-    # f = '4*x1^2 + 3*x2^2 - 4*x1*x2 + x1'
+    ############################################################
 
+    #n = int(input('Amount of variables: '))
+    
+    #f = input('f{0} = '.format(variables))
+    
+    #n = 2
+    #4*x1^2-6*x1*x2-34*x1+5*x2^2+42*x2+7
+    n = 3
+    f = 'x1^3 + x2^2 + x3^2 + x2*x3 - 3*x1 + 6*x2 + 2'
     variables = sym.symbols(', '.join([f'x{i}' for i in range(1, n+1)]))
-
+    
     partial_derivs = [sym.diff(f, variable) for variable in variables]
 
-    roots = sym.solve(
-        partial_derivs,
-        variables
-    )
-
-    if type(roots) == dict:
-        roots = [[float(value) for value in roots.values()]]
 
     print('\nPartial derivatives:', *partial_derivs, sep='\n')
-    print(f'\nPoints: ', *roots, sep='\n')
+    print('\nSystem of equations:')
+    
+    print(" __\n|")
+    for part_deriv in partial_derivs:
+        print(f'| {part_deriv} = 0')
+    print("|__")
+    
+    roots = sym.solve(
+        partial_derivs,
+        variables,
+        dict=True
+    )
+
+    if len(roots) == 0:
+        print("\nSystem of equations has no solutions!")
+        exit()
+    
+    print(f'\nPoints (system solutions): ', *roots, sep='\n')
     print()
 
-    # 4*x1^2 + 3*x2^2 - 4*x1*x2 + x1
-    # x1 ^ 3 + x2 ^ 2 + x3 ^ 2 + x2 * x3 - 3 * x1 + 6 * x2 + 2
-    # sympify(input_func).subs({'x': x}).evalf()
+    ############################################################
 
-    hessians = []
+    h = []
+    for part_deriv in partial_derivs:
+        row = []
+        for variable in variables:
+            second_pd = sym.diff(part_deriv, variable)
+            row.append(second_pd)
+        h.append(row)
+
+    dfhs = pd.DataFrame(h)
+    rows, cols = dfhs.shape
+
+    print('Hessian H:')
+    print(
+        tabulate(dfhs, tablefmt='pretty', showindex=False), '\n'
+    )
+    print('--------------------------------------------------')
+        
+    ############################################################
+
+    h_in_roots = []
     minor_values = []
 
     for root in roots:
-        hessian = []
-        for part_deriv in partial_derivs:
+        h_in_root = []
+        for i in range(rows):
             row = []
-            for variable in variables:
-                second_pd = sym.diff(part_deriv, variable)
-                if n == 2:
-                    row.append(
-                        sym.sympify(second_pd).subs({'x1': root[0], 'x2': root[1]})
-                    )
-                elif n == 3:
-                    row.append(
-                        sym.sympify(second_pd).subs({'x1': root[0], 'x2': root[1], 'x3': root[2]})
-                    )
+            for j in range(cols):
+                row.append(sym.sympify(dfhs.loc[i, j].subs(root)))
+            h_in_root.append(row)
 
-            hessian.append(row)
 
         minor = []
 
         for i in range(n):
-            matrix = np.array(hessian)
+            matrix = np.array(h_in_root)
             minor.append(
                 sym.Matrix(matrix[:i+1, :i+1]).det()
             )
 
         minor_values.append(minor)
-        hessians.append(hessian)
 
-    for i, hess in enumerate(hessians):
-        dfhs = pd.DataFrame(hess)
-        dfhs.index += 1
+        h_in_roots.append(h_in_root)
+
+    ############################################################
+
+    for i, h in enumerate(h_in_roots):
+        dfhs = pd.DataFrame(h)
+        
+        f_val = sym.sympify(f).subs(roots[i])
+
         print(f'H{roots[i]}:')
         print(
-            tabulate(dfhs, headers=range(1, n+1), tablefmt='pretty'), '\n'
+            tabulate(dfhs, tablefmt='pretty', showindex=False)
         )
-
-        print(f'Minor values: {minor_values[i]}')
+        
+        print(f'Minor values: {minor_values[i]}\n')
+        print(f'f({roots[i]}) = {f_val}\n')
 
         if all(v > 0 for v in minor_values[i]):
             print(f'Point {roots[i]} is MINIMUM point.\n')
@@ -94,6 +122,8 @@ def main():
             else:
                 print(f'Point {roots[i]} is a seddle point\n')
 
+        print('--------------------------------------------------')
+        
 
 if __name__ == '__main__':
     main()
